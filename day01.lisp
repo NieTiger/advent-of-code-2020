d38f69f48e93b9c0bf393fcdaca55f136e04e82e
Tiger Nie <nhl0819@gmail.com>
2020-12-02 15:47:32 +0800

    day01 part 2

--- day01.lisp
+++ day01.lisp
@@ -9,18 +9,35 @@
                  (cons line res)))
         ((eql line eof) (nreverse (cdr res)))))))
 
-(defun 2sum (lst &optional (sum 2020))
+(defun 2sum (lst sum)
   (do* ((mp (make-hash-table))
-       (l lst (cdr l))
-       (match nil (gethash (car l) mp)))
-      ((or match (null l))
-       (if match 
-           (values match (car l))
-           (values -1 -1)))
-      (setf (gethash (- sum (car l)) mp) (car l))))
+        (l lst (cdr l))
+        (match nil (gethash (car l) mp)))
+    ((or (not (null match)) (null l))
+     (if match 
+         (list match (car l))
+         nil))
+    (setf (gethash (- sum (car l)) mp) (car l))))
 
-(defun solution (lst)
-  (multiple-value-bind (a b) (2sum lst 2020)
-    (* a b)))
+(defun 3sum (lst sum)
+  (some (lambda (curr) 
+          (let ((2sum-res (2sum lst (- sum curr))))
+            (if 2sum-res
+                (cons curr 2sum-res)
+                nil)))
+        lst))
 
-(print (solution (load-input ifile)))
+(defun solution-part-1 (lst)
+  (let ((res (2sum lst 2020)))
+    (if res
+        (* (car res) (cadr res))
+        nil)))
+        
+(defun solution-part-2 (lst)
+  (let ((res (3sum lst 2020)))
+    (if res
+        (* (car res) (cadr res) (caddr res))
+        nil)))
+
+(format t "Part 1: ~A~%" (solution-part-1 (load-input ifile)))
+(format t "Part 2: ~A~%" (solution-part-2 (load-input ifile)))
--- day01.py
+++ day01.py
@@ -4,19 +4,41 @@ def twoSum(arr, sum):
         if a in d:
             return a, d[a]
         d[sum - a] = a
-    return -1, -1
+    return None
 
 
-def solution(arr, sum=2020):
+def threeSum(arr, sum):
+    arr.sort()
+    for a in arr:
+        res = twoSum(arr, sum - a)
+        if res:
+            return (a, *res)
+    return None
+
+
+def solutionPart1(arr, sum=2020):
     a, b = twoSum(arr, sum)
     return a * b
 
 
-if __name__ == "__main__":
+def solutionPart2(arr, sum=2020):
+    a, b, c = threeSum(arr, sum)
+    return a * b * c
 
-    with open("./day01input.txt") as f:
+
+def loadInput(path):
+    with open(path) as f:
         raw = f.read()
+    return [int(d) for d in raw.strip().split()]
 
-    data = [int(d) for d in raw.strip().split()]
-    s = solution(data)
-    print(s)
+
+if __name__ == "__main__":
+    path = "./day01input.txt"
+
+    data = loadInput(path)
+    s = solutionPart1(data)
+    print("Part 1: ", s)
+
+    data = loadInput(path)
+    s = solutionPart2(data)
+    print("Part 2: ", s)
